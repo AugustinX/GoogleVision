364c9cc0cd46c5e55d87025609ab29bf9a514013
AugustinX <xinyangxu@gmail.com>
2018-02-17 20:33:12 +0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import base64
+from gtts import gTTS
+
+KEY = "***input your google vision cloud key here***"
+
+#Notice we set the perameter "rb" instead of "r", since it read in byte, otherwise it reads in a code given by system default.
+img = open(r"C:\Users\Administrator\Pictures\honda.jpg", "rb")
+def encode_image(image):
+  image_content = image.read()
+  return base64.b64encode(image_content)
+
+
+#for the convience of debugging we fetch 3 results#
+req = {
+	"requests": {
+		"image":{
+			"content": encode_image(img).decode()
+		},
+		"features":{
+          "type":"LABEL_DETECTION",
+          "maxResults":3
+        }
+	}
+}
+
+
+
+
+data = json.dumps(req)
+r = requests.post("https://vision.googleapis.com/v1/images:annotate?key="+KEY, data)
+print (r.url)
+print (r.text)
+result = r.json()["responses"][0]["labelAnnotations"][0]["description"]
+
+tts = gTTS(text=result, lang='en', slow=False)
+tts.save("result.mp3")
+
